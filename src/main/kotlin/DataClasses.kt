ce0af6649cec0cb4c6fdcd48a0cbc6bb178fe1c4
A M Rizal <34767112+amrizal94@users.noreply.github.com>
2023-10-02 19:54:16 +0700

    Konsep Functional Programming

--- src/main/kotlin/DataClasses.kt
+++ src/main/kotlin/DataClasses.kt
@@ -11,8 +11,15 @@ data class DataUserIntro(val name: String, val age: Byte){
         println("My name $name, I am $age years old")
     }
 }
+val greething: (String) -> String =  {
+    "hello $it"
+}
+fun printName(value: String, name: (String) -> String){
+    println(name(value))
+}
 
 fun main() {
+
     val user1 = User("Umam", 17)
     val user2 = Users("Umam", 17)
     val dataUser1 = DataUser("Umam", 17)
@@ -27,4 +34,45 @@ fun main() {
     println("my name is $name, i am $age years age")
     val user5 = DataUserIntro(name, age)
     user5.intro()
+    println(greething("AMR"))
+    printName("Bhum"){
+        val double = it + it
+        "my name is $double"
+    }
+    val senin = "senin".firstAndLast()
+    val first = senin["first"]
+    val last = senin["last"]
+    println("huruf pertama \"$first\" dan huruf terakhir \"$last\"")
+    val double = 10.double
+    println(double)
+
+    val namaku = StringBuilder()
+    namaku.apply {
+        append("Ari ")
+        append("Muhammad ")
+        append("Rizal")
+    }
+    println(namaku)
+    val ganjil = 1..99 step 2
+    val acak = ganjil.take(ganjil.last).random()
+    println( acak)
+    val genap = (1..100 ).filter { it % 2 == 0 }
+    println(genap.random())
+    val userTank1 = UserTank("AMR")
+    userTank1.printName()
 }
+
+class UserTank(val name: String){
+    fun printName(){
+        println("my name is $name")
+    }
+}
+
+fun String.firstAndLast(): Map<String, Char>{
+    return mapOf(
+        "first" to first(), "last" to last()
+    )
+}
+
+val Int.double: Int
+    get() = this * 2
